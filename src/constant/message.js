dfdd94a3d3a66c7b8b27b4bdca26313337488e37
dharathakkarz <dharat@zignuts.com>
2024-05-01 18:40:39 +0530

    change

---
+++ src/constant/message.js
@@ -0,0 +1,13 @@
+export const message = {
+    DELETE:"Delete Profile successful!",
+    UPDATE:"Edit Profile successful!",
+    LIKE:'Profile Liked'
+
+}
+
+
+export const toaststyle={
+    position: 'top-center',
+    autoClose: 2000,
+    hideProgressBar: false
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 
-import React, { useState } from 'react';
+
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -24,6 +27,38 @@ import TextField from '@mui/material/TextField';
 import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
 
+// import { ToastContainer, toast } from 'material-react-toastify';
+// import 'material-react-toastify/dist/ReactToastify.css';
+
+
+
+function Home() {
+  const [prof, setProf] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => setProf(response.data))
+      .catch(error => console.error( error));
+  }, []);
+
+  const deleteProfile = (id) => {
+    setProf(prof.filter(user => user.id !== id)); 
+  };
+
+  const editProfile = (editedProfile) => {
+    setProf(prof.map(user => user.id === editedProfile.id ? editedProfile : user));
+  };
+
+  return (
+    <div style={{  marginTop:10 ,display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 10 }}>
+      {prof.map(user => (
+        <ProfileCard key={user.id} user={user} onDelete={deleteProfile} onEdit={editProfile} /> 
+      ))}
+    </div>
+  );
+}
+
+
 function ProfileCard({ user, onDelete, onEdit }) {
   const [Like, setLike] = useState(false);
   const [editOpen, seteditOpen] = useState(false);
@@ -38,18 +73,16 @@ function ProfileCard({ user, onDelete, onEdit }) {
 
   return (
     <Card sx={{ maxWidth: 345, marginRight: 2, marginBottom: 2 , }}>
-<CardMedia
-    sx={{
-      height: 160,
-      width: 160,
-      marginLeft: 5,
-      backgroundColor: 'lightgray !important' 
-    }}
-    image={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
-    title="Avatar"
-  />
-
-
+      <CardMedia
+        sx={{
+          height: 160,
+          width: 160,
+          marginLeft: 5,
+          backgroundColor: 'lightgray !important' 
+        }}
+        image={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+        title="Avatar"
+      />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {user.name}
@@ -60,14 +93,12 @@ function ProfileCard({ user, onDelete, onEdit }) {
             {user.email}
           </Typography>
         </Box>
-
         <Box display="flex" alignItems="center">
           <PhoneEnabledIcon sx={{ marginRight: 1 }} />
           <Typography variant="body2" color="text.secondary">
             {user.phone}
           </Typography>
         </Box>
-
         <Box display="flex" alignItems="center">
           <LanguageIcon sx={{ marginRight: 1 }} />
           <Typography variant="body2" color="text.secondary">
@@ -75,20 +106,18 @@ function ProfileCard({ user, onDelete, onEdit }) {
           </Typography>
         </Box>
       </CardContent>
-  
       <Box >
-  <BottomNavigation showLabels sx={{ backgroundColor: 'lightgray' }}>
-    <BottomNavigationAction
-      icon={Like ? <FavoriteIcon color='error' /> : <FavoriteBorderOutlinedIcon color='error' />}
-      onClick={likeBtn}
-    />
-    <Divider orientation="vertical" variant="middle" flexItem />
-    <BottomNavigationAction icon={<BorderColorOutlinedIcon />} onClick={editBtn} />
-    <Divider orientation="vertical" variant="middle" flexItem />
-    <BottomNavigationAction icon={<DeleteIcon />} onClick={() => onDelete(user.id)} />
-  </BottomNavigation>
-</Box>
-
+        <BottomNavigation showLabels sx={{ backgroundColor: 'lightgray' }}>
+          <BottomNavigationAction
+            icon={Like ? <FavoriteIcon color='error' /> : <FavoriteBorderOutlinedIcon color='error' />}
+            onClick={likeBtn}
+          />
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <BottomNavigationAction icon={<BorderColorOutlinedIcon />} onClick={editBtn} />
+          <Divider orientation="vertical" variant="middle" flexItem />
+          <BottomNavigationAction icon={<DeleteIcon />} onClick={() => onDelete(user.id)} />
+        </BottomNavigation>
+      </Box>
       <EditPopup isOpen={editOpen} onClose={editBtn} user={user} onEdit={onEdit} />
     </Card>
   );
@@ -111,63 +140,55 @@ function EditPopup({ isOpen, onClose, user, onEdit }) {
 
   const editProfile = () => {
     onEdit(editedProfile);
-    
     onClose();
   };
 
   return (
     <Dialog open={isOpen} onClose={handleClose}>
-      {/* <DialogTitle>Basic Model</DialogTitle> */}
       <DialogTitle>
-  Basic Model
-  <IconButton
-    aria-label="close"
-    onClick={handleClose}
-    sx={{
-      position: 'absolute',
-      right: 8,
-      top: 8,
-    }}
-  >
-    <CloseIcon />
-  </IconButton>
-</DialogTitle>
-
+        Basic Model
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
       <DialogContent >
-      <label htmlFor="email">Name:</label>
+        <label htmlFor="email">Name:</label>
         <TextField
-   
           fullWidth
           margin="normal"
-        id='email'
-        type='email'
+          id='email'
+          type='email'
           name="name"
           value={editedProfile.name}
           onChange={handleInputChange}
         />
-          <label htmlFor="email">Email:</label>
+        <label htmlFor="email">Email:</label>
         <TextField
           fullWidth
           margin="normal"
-          
           name="email"
           value={editedProfile.email}
           onChange={handleInputChange}
         />
-         
-       Phone: <TextField
+        Phone: <TextField
           fullWidth
           margin="normal"
-         
           name="phone"
           value={editedProfile.phone}
           onChange={handleInputChange}
         />
-          <label htmlFor="email">Website:</label>
+        <label htmlFor="email">Website:</label>
         <TextField
           fullWidth
           margin="normal"
-       
           name="website"
           value={editedProfile.website}
           onChange={handleInputChange}
@@ -178,41 +199,6 @@ function EditPopup({ isOpen, onClose, user, onEdit }) {
         <Button variant="contained" onClick={editProfile}>Ok</Button>
       </DialogActions>
     </Dialog>
-
   );
 }
-
-function Home() {
-  const [prof, setProf] = useState([]);
-
-  React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setProf(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  const deleteProfile = (id) => {
-    setProf(prof.filter(user => user.id !== id)); 
-  };
-
-  const editProfile = (editedProfile) => {
-    setProf(prof.map(user => user.id === editedProfile.id ? editedProfile : user));
-  };
-
-  return (
-    <div style={{  marginTop:10 ,display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 10 }}>
-      {prof.map(user => (
-        <ProfileCard key={user.id} user={user} onDelete={deleteProfile} onEdit={editProfile} /> 
-      ))}
-    </div>
-  );
-}
-
 export default Home; 
-
-
-
-
-
-
